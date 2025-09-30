e287855aa112b639f99117f06a04ad49c43b73bb
Bryan Miller <bmiller1008@gmail.com>
2025-09-30 14:13:08 -0400

    Platform updated. Almost works, but still bugs.

--- scripts/nodes/platform.gd
+++ scripts/nodes/platform.gd
@@ -23,6 +23,7 @@ enum Travel {UP_LEFT=-1, DOWN_RIGHT=1}
 @export var map_layer : TileMapLayer = null
 @export var initial_direction : Travel = Travel.DOWN_RIGHT
 @export_range(0.0, 4.0) var speed_scale : float = 1.0
+@export var enabled : bool = true:								set=set_enabled
 
 # ------------------------------------------------------------------------------
 # Variables
@@ -33,9 +34,18 @@ var _dir : Travel = Travel.DOWN_RIGHT
 var _progress : float = 0.0
 
 # ------------------------------------------------------------------------------
+# Setters
+# ------------------------------------------------------------------------------
+func set_enabled(e : bool) -> void:
+	if e != enabled:
+		enabled = e
+		set_physics_process(enabled)
+
+# ------------------------------------------------------------------------------
 # Override Methods
 # ------------------------------------------------------------------------------
 func _ready() -> void:
+	set_physics_process(enabled)
 	_dir = initial_direction
 	_Process(0.0, true)
 
@@ -45,89 +55,184 @@ func _physics_process(delta: float) -> void:
 # ------------------------------------------------------------------------------
 # Private Methods
 # ------------------------------------------------------------------------------
-func _FlipDirection() -> void:
+func _FlipDefaultDirection() -> void:
 	if _dir == Travel.UP_LEFT:
 		_dir = Travel.DOWN_RIGHT
 	else: _dir = Travel.UP_LEFT
 
+func _FlipDirection(dir : Travel) -> Travel:
+	if dir == Travel.UP_LEFT:
+		return Travel.DOWN_RIGHT
+	return Travel.UP_LEFT
+
+func _GetDirection(from : Vector2i, to : Vector2i) -> Travel:
+	if to.x < from.x or to.y < from.y:
+		return Travel.UP_LEFT
+	elif to.x > from.x or to.y > from.y:
+		return Travel.DOWN_RIGHT
+	return _dir
+
+func _UpdateCoords(initial : bool, skips : int) -> Rail:
+	var coord : Vector2i = map_layer.local_to_map(position)
+	var rail : Rail = _GetRailAtCoord(coord)
+	if not (rail == Rail.NONE or rail == Rail.CROSS):
+		if initial:
+			_cur_coord = coord
+			_prev_coord = coord
+		if coord != _cur_coord:
+			_prev_coord = _cur_coord
+			_cur_coord = coord
+		
+		if skips > 0:
+			var direction : Travel = _GetDirection(_prev_coord, _cur_coord)
+			for _i : int in range(skips):
+				var ncoord : Vector2i = _NextCellFromRail(_cur_coord, direction, rail)
+				var nrail : Rail = _GetRailAtCoord(ncoord)
+				if nrail == Rail.NONE:
+					_prev_coord = _cur_coord
+					_dir = _FlipDirection(direction)
+				else:
+					_prev_coord = _cur_coord
+					_cur_coord = ncoord
+					rail = nrail
+		
+	return rail
+
 func _Process(delta : float, initial : bool = false) -> void:
 	if map_layer == null: return
 	
-	var coord : Vector2i = map_layer.local_to_map(position)
-	var rail : Rail = _GetRailAtCoord(coord)
-	#print("Coord: ", coord, " | Cur: ", _cur_coord, " | Prev: ", _prev_coord)
-	if rail == Rail.NONE or rail == Rail.CROSS:
-		#print("No Rail")
-		return
-	#print("Rail: ", rail)
-	if initial: _cur_coord = coord
-	if coord != _cur_coord:
-		_prev_coord = _cur_coord
-		_cur_coord = coord
+	_progress += delta * speed_scale
+	var skips : int = abs(floor(_progress))
+	_progress = _progress - floor(_progress)
 	
-	var nprog : float = _progress + (delta * speed_scale * _dir)
-	var skips : int = abs(floor(nprog))
-	for _i : int in range(skips):
-		# TODO: Shit'll get weird if rail is Rail.CROSS
-		var ncoord : Vector2i = _NextCellFromRail(_cur_coord, _dir, rail)
-		var nrail : Rail = _GetRailAtCoord(ncoord)
-		if nrail == Rail.NONE:
-			_prev_coord = _cur_coord
-			_FlipDirection()
-		else:
-			_prev_coord = _cur_coord
-			_cur_coord = ncoord
-			rail = nrail
-	
-	_progress = nprog - floor(nprog)
-	if _progress >= 0.98 and _progress <= 1.0:
-		print("Blah")
-	var progress : float = _progress
-	if _cur_coord.x < _prev_coord.x or _cur_coord.y < _prev_coord.y:
-		progress = 1.0 - _progress
-	
+	var rail : Rail = _UpdateCoords(initial, skips)
+	if rail == Rail.NONE or rail == Rail.CROSS: return
+	var direction : Travel = _GetDirection(_prev_coord, _cur_coord)
+
 	match rail:
 		Rail.HORIZONTAL:
-			_ProcessHorizontal(_cur_coord, progress)
+			_PositionAlongHorizontal(direction)
 		Rail.VERTICAL:
-			_ProcessVertical(_cur_coord, progress)
+			_PositionAlongVertical(direction)
 		Rail.BOTTOM_RIGHT:
-			if _progress <= 0.5:
-				_ProcessVertical(_cur_coord, 1.0 - progress)
-			else:
-				_ProcessHorizontal(_cur_coord, progress)
+			_PositionCorner(direction, false, false)
 		Rail.BOTTOM_LEFT:
-			if _progress <= 0.5:
-				_ProcessHorizontal(_cur_coord, progress)
-			else:
-				_ProcessVertical(_cur_coord, progress)
+			_PositionCorner(direction, true, false)
 		Rail.TOP_LEFT:
-			if _progress <= 0.5:
-				_ProcessHorizontal(_cur_coord, progress)
-			else:
-				_ProcessVertical(_cur_coord, 1.0 - progress)
+			_PositionCorner(direction, true, true)
 		Rail.TOP_RIGHT:
-			if _progress <= 0.5:
-				_ProcessVertical(_cur_coord, progress)
-			else:
-				_ProcessHorizontal(_cur_coord, progress)
+			_PositionCorner(direction, false, true)
 
+func _PositionCorner(direction : Travel, h_left : bool, v_up : bool) -> void:
+	if _progress <= 0.5:
+		if h_left:
+			_PositionAlongHorizontal(direction)
+		else:
+			var flip : bool = v_up and direction == Travel.UP_LEFT
+			_PositionAlongVertical(_FlipDirection(direction) if flip else direction)
+	else:
+		if not h_left:
+			_PositionAlongHorizontal(direction)
+		else:
+			var flip : bool = v_up and direction == Travel.DOWN_RIGHT
+			_PositionAlongVertical(_FlipDirection(direction) if flip else direction)
 
-func _ProcessHorizontal(coord : Vector2i, progress : float) -> void:
+func _PositionAlongHorizontal(direction : Travel) -> void:
 	if map_layer == null or map_layer.tile_set == null: return
+	var progress : float = _progress if direction == Travel.DOWN_RIGHT else 1.0 - _progress
 	var tile_size : Vector2i = map_layer.tile_set.tile_size
-	var pos : Vector2 = map_layer.map_to_local(coord) - (tile_size * 0.5)
-	var dist : float = float(tile_size.x) * progress
+	var pos : Vector2 = map_layer.map_to_local(_cur_coord) - (tile_size * 0.5)
+	var dist : float = float(tile_size.x - 1) * progress
 	var npos = pos + Vector2(dist, floor(float(tile_size.y) * 0.5))
 	position = npos
 
-func _ProcessVertical(coord : Vector2i, progress : float) -> void:
+func _PositionAlongVertical(direction : Travel) -> void:
 	if map_layer == null or map_layer.tile_set == null: return
+	var progress : float = _progress if direction == Travel.DOWN_RIGHT else 1.0 - _progress
 	var tile_size : Vector2i = map_layer.tile_set.tile_size
-	var pos : Vector2 = map_layer.map_to_local(coord) - (tile_size * 0.5)
-	var dist : float = float(tile_size.y) * progress
+	var pos : Vector2 = map_layer.map_to_local(_cur_coord) - (tile_size * 0.5)
+	var dist : float = float(tile_size.y - 1) * progress
 	var npos = pos + Vector2(floor(float(tile_size.x) * 0.5), dist)
 	position = npos
+
+#func _Process(delta : float, initial : bool = false) -> void:
+	#if map_layer == null: return
+	#
+	#var coord : Vector2i = map_layer.local_to_map(position)
+	#var rail : Rail = _GetRailAtCoord(coord)
+	#print("Coord: ", coord, " | Cur: ", _cur_coord, " | Prev: ", _prev_coord)
+	#if rail == Rail.NONE or rail == Rail.CROSS:
+		##print("No Rail")
+		#return
+	#print("Rail: ", rail)
+	#if initial: _cur_coord = coord
+	#if coord != _cur_coord:
+		#_prev_coord = _cur_coord
+		#_cur_coord = coord
+	#
+	#var nprog : float = _progress + (delta * speed_scale * _dir)
+	#var skips : int = abs(floor(nprog))
+	#for _i : int in range(skips):
+		## TODO: Shit'll get weird if rail is Rail.CROSS
+		#var ncoord : Vector2i = _NextCellFromRail(_cur_coord, _dir, rail)
+		#var nrail : Rail = _GetRailAtCoord(ncoord)
+		#if nrail == Rail.NONE:
+			#_prev_coord = _cur_coord
+			#_FlipDirection()
+		#else:
+			#_prev_coord = _cur_coord
+			#_cur_coord = ncoord
+			#rail = nrail
+	#
+	#_progress = nprog - floor(nprog)
+	#if _progress >= 0.98 and _progress <= 1.0:
+		#print("Blah")
+	#var progress : float = _progress
+	#if _cur_coord.x < _prev_coord.x or _cur_coord.y < _prev_coord.y:
+		#progress = 1.0 - _progress
+	#
+	#match rail:
+		#Rail.HORIZONTAL:
+			#_ProcessHorizontal(_cur_coord, progress)
+		#Rail.VERTICAL:
+			#_ProcessVertical(_cur_coord, progress)
+		#Rail.BOTTOM_RIGHT:
+			#if _progress <= 0.5:
+				#_ProcessVertical(_cur_coord, 1.0 - progress)
+			#else:
+				#_ProcessHorizontal(_cur_coord, progress)
+		#Rail.BOTTOM_LEFT:
+			#if _progress <= 0.5:
+				#_ProcessHorizontal(_cur_coord, progress)
+			#else:
+				#_ProcessVertical(_cur_coord, progress)
+		#Rail.TOP_LEFT:
+			#if _progress <= 0.5:
+				#_ProcessHorizontal(_cur_coord, progress)
+			#else:
+				#_ProcessVertical(_cur_coord, 1.0 - progress)
+		#Rail.TOP_RIGHT:
+			#if _progress <= 0.5:
+				#_ProcessVertical(_cur_coord, progress)
+			#else:
+				#_ProcessHorizontal(_cur_coord, progress)
+
+
+#func _ProcessHorizontal(coord : Vector2i, progress : float) -> void:
+	#if map_layer == null or map_layer.tile_set == null: return
+	#var tile_size : Vector2i = map_layer.tile_set.tile_size
+	#var pos : Vector2 = map_layer.map_to_local(coord) - (tile_size * 0.5)
+	#var dist : float = float(tile_size.x - 1) * progress
+	#var npos = pos + Vector2(dist, floor(float(tile_size.y) * 0.5))
+	#position = npos
+#
+#func _ProcessVertical(coord : Vector2i, progress : float) -> void:
+	#if map_layer == null or map_layer.tile_set == null: return
+	#var tile_size : Vector2i = map_layer.tile_set.tile_size
+	#var pos : Vector2 = map_layer.map_to_local(coord) - (tile_size * 0.5)
+	#var dist : float = float(tile_size.y - 1) * progress
+	#var npos = pos + Vector2(floor(float(tile_size.x) * 0.5), dist)
+	#position = npos
 
 
 func _GetRailAtCoord(coord : Vector2i) -> Rail:
@@ -147,8 +252,8 @@ func _NextCellFromRail(cell : Vector2i, direction : Travel, rail : Rail, prev_ra
 			cell.y += direction
 		Rail.BOTTOM_RIGHT:
 			if direction == Travel.DOWN_RIGHT:
-				cell.y += 1
-			else: cell.x -= 1
+				cell.x += 1
+			else: cell.y += 1
 		Rail.BOTTOM_LEFT:
 			if direction == Travel.DOWN_RIGHT:
 				cell.y += 1
